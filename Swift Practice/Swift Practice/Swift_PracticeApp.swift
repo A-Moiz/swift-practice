0796495621a372853682d586e9da55f40e945a83
Abdul Moiz <abdulmoiz@MacBookAir-2.powerhub>
2026-09-25 23:53:53 +0100

    Created Xcode project

---
+++ Swift Practice/Swift Practice/Swift_PracticeApp.swift
@@ -0,0 +1,17 @@
+//
+//  Swift_PracticeApp.swift
+//  Swift Practice
+//
+//  Created by Abdul Moiz on 25/09/2026.
+//
+
+import SwiftUI
+
+@main
+struct Swift_PracticeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
